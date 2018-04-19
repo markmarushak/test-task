b8942d4b25751280f7ff1cd19c397d37e05f2cd6
markmarushak <marusahk.web.master@gmail.com>
2018-04-19 22:59:32 +0300

    asdasd

--- ajax.php
+++ ajax.php
@@ -1,2 +1,10 @@
 <?php 
 
+if(!empty($_POST))
+{
+    return [
+        'name' => $_POST['name'],
+        'date' => $_POST['date'],
+        'text' => $_POST['text']
+    ];
+}
--- assets/js/common.js
+++ assets/js/common.js
@@ -1,16 +1,35 @@
 $(document).ready(function() {
+	var t = moment().locale('ru').format('DD MMMM YYYY');
 
-	$('.sender-button').click(function (event) {
+    $('input#date').val(t);
+
+    $('#form-comments').submit(function (event) {
 		event.preventDefault();
 
-		var data = $('form').serialize();
+		var data = $('#form-comments').serializeArray();
 
-		console.log(data);
 		$.ajax({
-			url: ajax.php,
+			url: '',
 			data: data,
-			success:function (data) {
-				alert('good');
+			success:function () {
+				$('.comment > div ').append(
+					'<div class="row animated fadeInUp">\n' +
+                    '\t\t\t\t<div class="col">\n' +
+                    '\t\t\t\t\t<div class="comment-user">\n' +
+                    '\t\t\t\t\t\t<div class="comment-user-name">\n' +
+                    '\t\t\t\t\t\t\t<strong>'+ data[0].value +
+					'</strong>\n' +
+                    '\t\t\t\t\t\t\t<div class="comment-user-data">'+ data[1].value +
+					'</div>\n' +
+                    '\t\t\t\t\t\t</div>\n' +
+                    '\t\t\t\t\t</div>\n' +
+                    '\t\t\t\t</div>\n' +
+                    '\t\t\t\t<div class="col-12">\n' +
+                    '\t\t\t\t\t<div class="comment-text">'+ data[2].value+
+					'</div>\n' +
+                    '\t\t\t\t</div>\n' +
+                    '\t\t\t</div>'
+				)
 			}
 		});
 	});
